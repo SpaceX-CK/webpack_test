752270652c699c8b57953a2af418563318829f88
ck <spacex.ck@gmail.com>
2021-12-26 05:26:39 +0800

    update urloader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 console.log('Hello World');
+import './css/first.css';
 // import './images/ins.jpg';
+// "browserlist": {
+//     "development": [
+//       "last 2 Chrome versions",
+//       "last 2 Firefox versions",
+//       "last 2 Safari versions",
+//       "not dead",
+//       "not op_mini all"
+//     ],
+//     "production": [
+//       "> 0.2%",
+//       "not dead",
+//       "not op_mini all"
+//     ]
+//   }
--- webpack.config.js
+++ webpack.config.js
@@ -14,63 +14,88 @@ module.exports = {
     module: {
         //loader "use" excecutes from top to bottom
         rules: [
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         // creates style nodes from JS strings
+            //         "style-loader",
+            //         //css changes to a comonjs module imported in the index.js
+            //         "css-loader",
+            //         // options: {
+            //         //     outputPath: "css",
+            //         // }
+            //     ],
+            // },
             {
                 test: /\.css$/,
                 use: [
-                    // creates style nodes from JS strings
+                    // loader.MiniCssExtractPlugin.loader,
                     "style-loader",
-                    //css changes to a comonjs module imported in the index.js
                     "css-loader",
+                    // {
+                    //     options: {
+                    //         outputPath: "css",
+                    //         // localIdentName: "[name]__[local]--[hash:base64:5]",
+                    //     },
+                    // },
                 ],
             },
-            // {
-            //     // test: /\.(png|jpe?g|gif)$/i,
-            //     test: /\.(png|jpg|gif)$/,
-            //     loader: 'file-loader',
-            //     options : {
-            //         // outputPath: 'images',
-            //         esModule: false,
-            //         // output file name
-            //         // [name] is the name of the file
-            //         // [ext] is the extension of the file
-            //         // name: 'images/[name].[ext]'
-            //         // hash is a unique hash for the file limit 10 chars
-            //         // name: '[name].[ext]',
-            //         name: '[hash:10].[ext]'
-            //     }
-            // },
-          
             {
+                // test: /\.(png|jpe?g|gif)$/i,
+                // test: /\.(png|jpg|gif)$/,
                 test: /\.(svg|png|jpg|jpeg|gif)$/,
-                // include: 'images',
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        esModule: false,
-                        // name: 'images/[name].[ext]',
-                        name: "[name].[ext]",
-                        outputPath: "images",
-                        // name: 'images/[hash:6].[ext]'
-                    },
-                },
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            esModule: false,
+                            limit: 8 * 1024,
+                            name: "images/[name][hash:6].[ext]",
+                        },
+                    }
+                ]
+                // loader: 'url-loader',
+                // options: {
+                //     esModule: false,
+                //     // less than 8kb ==> data url conversion (base64)
+                //     limit: 8 * 1024,
+                //     // [name] is the name of the file
+                //     // [ext] is the extension of the file
+                //     outputPath: "images",
+                //     name: '[hash:6].[ext]'
+                //     // name: '[name].[ext]'
+                // }
             },
+            // {
+            //     test: /\.(svg|png|jpg|jpeg|gif)$/,
+            //     // include: 'images',
+            //     use: {
+            //         loader: "file-loader",
+            //         options: {
+            //             esModule: false,
+            //             // name: 'images/[name].[ext]',
+            //             name: "[name].[ext]",
+            //             outputPath: "images",
+            //             // name: 'images/[hash:6].[ext]'
+            //         },
+            //     },
+            // },
             {
                 test: /\.html$/,
                 loader: "html-loader",
                 options: {
                     // Disables attributes processing
                     sources: false,
-                  },
+                },
             },
 
-            // {
-            //     exclude: /\.(html|js|css|svg|png|jpg|jpeg|gif)$/,
-            //     loader: 'file-loader',
-            //     options: {
-            //         name: '[name].[ext]',
-            //         // outputPath: '/images/'
-            //     }
-            // }
+            {
+                exclude: /\.(html|js|css|svg|png|jpg|jpeg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: "source",
+                }
+            }
         ],
     },
     // plugins is where you can add plugins
@@ -84,4 +109,19 @@ module.exports = {
 
     // mode is used to determine what kind of bundle is created
     mode: "development",
+
+    //activate dev server : npx webpack-dev-server
+    devServer: {    // webpack-dev-server
+        // only cache the bundle file not the whole project
+        // contentBase: resolve(__dirname, 'build'),
+        // open the default browser
+        open: true,
+        // port number
+        port: 9000,
+        //gzipped bundle file
+        compress: true,
+        // open the browser
+        open: true,
+    }
+
 };
