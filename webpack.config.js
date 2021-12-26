d2dd7c99a5f44c7444b271fa10f993586eb5ecf8
ck <spacex.ck@gmail.com>
2021-12-27 02:20:49 +0800

    update minicssExtrctplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,127 +1,115 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { resolve } = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
-    // output is where the bundle file will be created
-    output: {
-        filename: "main.js",
-        // path: __dirname + '/build',
-        // assetModuleFilename: 'images/[name][ext]'
-        path: resolve(__dirname, "build"),
-    },
-    // module is where you can add loaders
-    module: {
-        //loader "use" excecutes from top to bottom
-        rules: [
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         // creates style nodes from JS strings
-            //         "style-loader",
-            //         //css changes to a comonjs module imported in the index.js
-            //         "css-loader",
-            //         // options: {
-            //         //     outputPath: "css",
-            //         // }
-            //     ],
-            // },
-            {
-                test: /\.css$/,
-                use: [
-                    // loader.MiniCssExtractPlugin.loader,
-                    "style-loader",
-                    "css-loader",
-                    // {
-                    //     options: {
-                    //         outputPath: "css",
-                    //         // localIdentName: "[name]__[local]--[hash:base64:5]",
-                    //     },
-                    // },
-                ],
-            },
-            {
-                // test: /\.(png|jpe?g|gif)$/i,
-                // test: /\.(png|jpg|gif)$/,
-                test: /\.(svg|png|jpg|jpeg|gif)$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            esModule: false,
-                            limit: 8 * 1024,
-                            name: "images/[name][hash:6].[ext]",
-                        },
-                    }
-                ]
-                // loader: 'url-loader',
-                // options: {
-                //     esModule: false,
-                //     // less than 8kb ==> data url conversion (base64)
-                //     limit: 8 * 1024,
-                //     // [name] is the name of the file
-                //     // [ext] is the extension of the file
-                //     outputPath: "images",
-                //     name: '[hash:6].[ext]'
-                //     // name: '[name].[ext]'
-                // }
-            },
-            // {
-            //     test: /\.(svg|png|jpg|jpeg|gif)$/,
-            //     // include: 'images',
-            //     use: {
-            //         loader: "file-loader",
-            //         options: {
-            //             esModule: false,
-            //             // name: 'images/[name].[ext]',
-            //             name: "[name].[ext]",
-            //             outputPath: "images",
-            //             // name: 'images/[hash:6].[ext]'
-            //         },
-            //     },
-            // },
-            {
-                test: /\.html$/,
-                loader: "html-loader",
-                options: {
-                    // Disables attributes processing
-                    sources: false,
-                },
-            },
-
-            {
-                exclude: /\.(html|js|css|svg|png|jpg|jpeg|gif)$/,
-                loader: 'file-loader',
-                options: {
-                    outputPath: "source",
-                }
-            }
+  entry: "./src/index.js",
+  // output is where the bundle file will be created
+  output: {
+    filename: "main.js",
+    // path: __dirname + '/build',
+    // assetModuleFilename: 'images/[name][ext]'
+    path: resolve(__dirname, "build"),
+  },
+  // module is where you can add loaders
+  module: {
+    //loader "use" excecutes from top to bottom
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          // "style-loader",
+          // miniCssExtractPlugin.loader replaces style-loader
+          // miniCssExtractPlugin.loader is a loader that extracts CSS into separate files.
+          MiniCssExtractPlugin.loader,
+          "css-loader",
         ],
-    },
-    // plugins is where you can add plugins
-    plugins: [
-        // creates an index.html file in the build folder auto plug in JS and CSS
-        new HtmlWebpackPlugin({
-            // use template to create the index.html file
-            template: "./src/index.html",
-        }),
+      },
+      // {
+      //     // test: /\.(png|jpe?g|gif)$/i,
+      //     test: /\.(svg|png|jpg|jpeg|gif)$/,
+      //     use: [
+      //         {
+      //             loader: "url-loader",
+      //             options: {
+      //                 esModule: false,
+      //                 limit: 8 * 1024,
+      //                 name: "images/[name][hash:3].[ext]",
+      //             },
+      //         }
+      //     ]
+      // },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        // type: "asset/font",
+        type: "asset/resource",
+        generator: {
+          fileName: "fonts/[name][hash:3].[ext]",
+        },
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/,
+        type: "asset",
+        generator: {
+          filename: "images/[name].[ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 1024 * 8, // 8kb
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          // Disables attributes processing
+          sources: false,
+        },
+      },
+      {
+        // exclude: /\.(html|js|css|svg|png|jpg|jpeg|gif)$/,
+        exclude: [/(^|\.(js|jsx|ts|tsx|html|css|svg|png|jpg|jpeg|gif))$/],
+        // loader: "file-loader",
+        type: 'asset/resource',
+        // options: {
+        //   outputPath: "source",
+        // },
+      },
     ],
+  },
+  plugins: [
+    // creates an index.html file in the build folder auto plug in JS and CSS
+    new HtmlWebpackPlugin({
+      // use template to create the index.html file
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "css/[name][hash:6].css" }),
+  ],
 
-    // mode is used to determine what kind of bundle is created
-    mode: "development",
+  // mode is used to determine what kind of bundle is created
+  mode: "development",
 
-    //activate dev server : npx webpack-dev-server
-    devServer: {    // webpack-dev-server
-        // only cache the bundle file not the whole project
-        // contentBase: resolve(__dirname, 'build'),
-        // open the default browser
-        open: true,
-        // port number
-        port: 9000,
-        //gzipped bundle file
-        compress: true,
-        // open the browser
-        open: true,
-    }
-
+  //activate dev server : npx webpack-dev-server
+  devServer: {
+    // webpack-dev-server
+    // only cache the bundle file not the whole project
+    // contentBase: resolve(__dirname, 'build'),
+    // open the default browser
+    open: true,
+    // port number
+    port: 9000,
+    //gzipped bundle file
+    compress: true,
+    // open the browser
+    open: true,
+  },
 };
+/**
+ * WEBPACK CONFIGURATION 5 
+ * asset module type
+ * 01 asset/resource-->flie-loader()
+ * 02 asset/inline-->url-loader() to all the images
+ * 03 asset/source-->raw-loader()
+ * 04 asset --> (parser  dataUrlCondition maxsize) dynamic import
+ */
